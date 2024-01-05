7cea1d3f9f4195d761563013fe171c24ec336a7b
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-01-05 22:15:59 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jigmet stanzin Dadul on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
